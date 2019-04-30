81ab0f95f4d7200d4a64fb8fd7db2dfc881bfaa5
Marco Baxemyr <baxemyr@gmail.com>
2019-04-30 12:41:37 +0200

    Initial commit, featuring class skeletons.

---
+++ ArenaFighter/Characters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaFighter.Character
+{
+    internal abstract class Character
+    {
+        
+    }
+
+
+    internal class Player : Character
+    {
+
+    }
+
+    internal class Gladiator : Character
+    {
+
+    }
+
+    internal class Novice : Character
+    {
+
+    }
+}
